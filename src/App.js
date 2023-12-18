c93c4a2fe8beab01518cbd48c9d2663deedfa9d9
Athipat Chumnanpa <Athipat.c@frt.local>
2023-12-18 15:09:11 +0700

    add search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from './Home';
 import Nav from './Nav';
+import Search from './Search';
 
 function App() {
 
   return (
     <div className="App">
       <Nav></Nav>
+      <Search></Search>
       <Home></Home>
     </div>
   );
