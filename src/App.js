0f397a7c31d5938037b671d065444e4b813873ca
tingtong <athipaty@gmail.com>
2023-12-23 16:54:49 +0700

    test..

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import Home from './Home';
-import Nav from './Nav';
-import Search from './Search';
-
 function App() {
-
   return (
     <div className="App">
-      <Nav></Nav>
-      <Search></Search>
-      <Home></Home>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eveniet modi iure maxime quidem laboriosam ratione tenetur neque fuga, esse quis voluptatum? Aliquid reiciendis quasi ipsa temporibus aut. Nesciunt, inventore.</p>
+    </div>
+  );
+}
+
+function Footer () {
+  return (
+    <div className="footer">
+      <p>&copy; TongTong {new Date().getFullYear()}</p>
     </div>
   );
 }
