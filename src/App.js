e6942dbce0c55f31c822419593eb8a24784a91d4
tingtong <athipaty@gmail.com>
2024-01-08 12:53:22 +0700

    Hello World!

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
+import "./App.css"
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div className="container">
       <h1>Hello World!</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eveniet modi iure maxime quidem laboriosam ratione tenetur neque fuga, esse quis voluptatum? Aliquid reiciendis quasi ipsa temporibus aut. Nesciunt, inventore.</p>
     </div>
   );
 }
 
-function Footer () {
-  return (
-    <div className="footer">
-      <p>&copy; TongTong {new Date().getFullYear()}</p>
-    </div>
-  );
-}
 
 export default App;
