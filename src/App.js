d2ad64c03d06af481cc418562db15d32b54bad13
tingtong <athipaty@gmail.com>
2024-01-08 13:31:49 +0700

    add axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css"
+import { wait } from "@testing-library/user-event/dist/utils";
 function App() {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const getData = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=c8bebd847d4df8b92eeedfa23986fc0c");
+        setMovies(getData.data.results);
+      } catch (error) {
+        console.log("something wend wrong" + error)
+      }
+    }
+    fetchData();
+  }, [])
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      {movies.map((item) => (
+        <div className="singleMovie" key={item.id}>
+          <h3>{item.title}</h3>
+          <img
+              width={"200px"}
+              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+              alt={item.title}
+            />
+          <p>{item.overview}</p>
+          <button>Favorite</button>
+        </div>
+
+      ))}
     </div>
   );
 }
